a0150b582eae9f679d5685ef5435edbf02aee688
ospinaospinacristian@gmail.com <ospinaospinacristian@gmail.com@6395b7b1-b687-a63c-7315-bfe4c860ec1a>
2014-07-21 06:45:26 +0000

    Finalizo los metodos de TipoDocumento y sus Test.

--- trunk/trunk/RTF2/src/com/edu/ude/test/TipoDocumentoDAOHibernateTest.java
+++ trunk/trunk/RTF2/src/com/edu/ude/test/TipoDocumentoDAOHibernateTest.java
@@ -28,11 +28,74 @@ public class TipoDocumentoDAOHibernateTest {
 		try{
 			TipoDocumento tipoDocumento= tipoDocumentoDAO.obtenerTipoDocumento("Cedula");
 			System.out.println("Tipo Documento: " + tipoDocumento.getTipoDocumento() + " Descripcion: "+ tipoDocumento.getDescripcion());
-			
+			assertTrue(true);
 		} catch (DAOException e){
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
+	
+	@Test
+	public void testObtenerTiposDocumento() {
+		System.out.println("__________________________________________________");
+		System.out.println("Corriendo Test para Obtener todos los tipos de documento");
+		try{
+			for(TipoDocumento tipoDocumento:tipoDocumentoDAO.obtenerTipoDocumento()){
+				System.out.println("Tipo Documento: " + tipoDocumento.getTipoDocumento() + " Descripcion: "+ tipoDocumento.getDescripcion());
+			}		
+			assertTrue(true);
+		} catch (DAOException e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	/*
+	@Test
+	public void testAgregarTiposDocumento() {
+		System.out.println("__________________________________________________");
+		System.out.println("Corriendo Test para Agregar un tipo de documento");
+		try{
+			TipoDocumento tipoDocumento= new TipoDocumento("Pasaporte", "Usado por investigadores extranjeros");
+			tipoDocumentoDAO.AgregarTipoDocumento(tipoDocumento);				
+			assertTrue(true);
+		} catch (DAOException e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+	
+	@Test
+	public void testModificarTiposDocumento() {
+		System.out.println("__________________________________________________");
+		System.out.println("Corriendo Test para Modificar un tipo de documento");
+		try{
+			TipoDocumento tipoDocumento=tipoDocumentoDAO.obtenerTipoDocumento("Pasaporte");
+			tipoDocumento.setDescripcion("Descripcion actualizada desde el test");
+			tipoDocumentoDAO.ActualizarTipoDocumento(tipoDocumento);				
+			assertTrue(true);
+		} catch (DAOException e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	public void testEliminarTiposDocumento() {
+		System.out.println("__________________________________________________");
+		System.out.println("Corriendo Test para Eliminar un tipo de documento");
+		try{
+			TipoDocumento tipoDocumento=tipoDocumentoDAO.obtenerTipoDocumento("Pasaporte");
+			tipoDocumentoDAO.EliminarTipoDocumento(tipoDocumento);				
+			assertTrue(true);
+		} catch (DAOException e){
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+	
+	*/
 
 }
