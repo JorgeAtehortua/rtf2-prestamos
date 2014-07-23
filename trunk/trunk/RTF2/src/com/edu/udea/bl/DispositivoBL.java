ea3640fe91a0d4c376b0995559c8cc971eb16990
ospinaospinacristian@gmail.com <ospinaospinacristian@gmail.com@6395b7b1-b687-a63c-7315-bfe4c860ec1a>
2014-07-23 08:41:15 +0000

    Se comenzo la creacion de la capa del negocio con la clase dispositivo.

--- trunk/trunk/RTF2/src/com/edu/udea/bl/DispositivoBL.java
+++ trunk/trunk/RTF2/src/com/edu/udea/bl/DispositivoBL.java
@@ -2,22 +2,24 @@ package com.edu.udea.bl;
 
 import java.util.List;
 
+import com.edu.udea.dao.DispositivoDAO;
 import com.edu.udea.dto.Dispositivo;
 import com.edu.udea.dto.TipoDispositivo;
+import com.edu.udea.util.exception.BLException;
 import com.edu.udea.util.exception.DAOException;
 
 public interface DispositivoBL {
+		
+	public Dispositivo obtenerDispositivo(Integer idDispositivo) throws DAOException, BLException;
 	
-	public Dispositivo obtenerDispositivo(Integer idDispositivo) throws DAOException;
-	
-	public List<Dispositivo> obtenerDispositivos() throws DAOException;
+	public List<Dispositivo> obtenerDispositivos() throws DAOException, BLException;
 	
 	public void agregarDispositivo(int idDispositivo, String nombreDispositivo,
 			String descripcionDispositivo, int cantidad, int disponibles,
-			TipoDispositivo tipoDispositivo) throws DAOException;
+			TipoDispositivo tipoDispositivo) throws DAOException, BLException;
 	
-	public void eliminarDispositivo(Integer idDispositivo) throws DAOException;
+	public void eliminarDispositivo(Integer idDispositivo) throws DAOException, BLException;
 	
-	public void actualizarDispositivo(Integer idDispositivo) throws DAOException;
+	public void actualizarDispositivo(Integer idDispositivo) throws DAOException, BLException;
 
 }
